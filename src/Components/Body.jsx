0377bb7b18e7b7fd72034be5cb0f1d705e21593a
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-06-28 22:28:10 +0530

    header part done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -7,7 +7,7 @@ const Body = () => {
         <img src="./assets/uixLogo.png" alt="" />
       </div>
       <div>
-        <p className="font-Fjalla size-10 flex">
+        <p className="font-Fjalla size-10  ">
           UiX is a Upcoming Digital Marketing company which will provide Digital
           growth ...{" "}
         </p>
