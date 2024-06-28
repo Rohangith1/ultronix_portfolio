d2292d6d4b8f395d0634b0361d07cbc53abc3b84
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-06-29 02:20:19 +0530

    mobile view done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,15 +2,38 @@ import React from "react";
 
 const Body = () => {
   return (
-    <div>
-      <div>
-        <img src="./assets/uixLogo.png" alt="" />
+    <div className="space-y-4">
+      <div className="flex items-center justify-center">
+        <img className="" src="./assets/uixLogo.png" alt="" />
       </div>
-      <div>
-        <p className="font-Fjalla size-10  ">
+      <div className="">
+        <h1 className="text-5xl font-Fjalla font-bold leading-tight bg-gradient-to-r from-zinc-500 via-amber-400 to-gray-900 bg-clip-text text-transparent ">
+          What is UiX?
+        </h1>
+        <p className="font-Fjalla text-gray-600 gap-5 ">
           UiX is a Upcoming Digital Marketing company which will provide Digital
-          growth ...{" "}
+          growth ... <br />
+          Connect with us to create a great network.
         </p>
+        <form action="" className="flex flex-col gap-5">
+          <input
+            type="email"
+            className="rounded-md px-5 py-4 font-Fjalla italic"
+            placeholder="Enter Your Email"
+            name=""
+            id=""
+          />
+          <button
+            className="bg-gradient-to-r from-slate-700 via-gray-400 to-slate-600 hover:from-pink-500 hover:to-yellow-500 text-s text-white rounded-md px-4 py-3 font-Fjalla transition duration-150 ease-in-out "
+            type="submit"
+          >
+            Send
+          </button>
+          <div className="gap-5 flex font-Fjalla">
+            <input type="checkbox" className="size-5 " name="" id="" />
+            <p>Get updates on Email.</p>
+          </div>
+        </form>
       </div>
       <div></div>
     </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer </div>
-  )
-}
+    <div className="flex flex-col gap-4">
+      <h1 className="font-RobotoMono text-2xl font-bold">Reach me out...</h1>
+      <ul className="flex flex-row gap-4 text-3xl ">
+        <li>
+          <a href="">
+            <AiFillGithub />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FaLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <RiInstagramFill />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FaXTwitter />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
